e4670354a60001dfe60f0394df4fa22a1e150bec
Narayanan Muthuswamy <narayanan_vm@apple.com>
2020-10-24 19:13:42 -0500

    Initial Commit

---
+++ Convert/ConvertApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConvertApp.swift
+//  Convert
+//
+//  Created by Narayanan V Muthuswamy on 10/24/20.
+//
+
+import SwiftUI
+
+@main
+struct ConvertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
